d7bde8bad29f37c86392a67d9b9a1c0879a7c902
Neibo Augustine <neiboaugustine@gmail.com>
2022-05-20 08:46:25 +0100

    id generation fixed

---
+++ ticket.js
@@ -0,0 +1,13 @@
+//  This is the code to generate unique id for booking
+let btn_id =document.getElementById('btn-id');
+let ticket_id = document.getElementById('ticket-id');
+
+btn_id.addEventListener('click',(e)=>{
+    e.preventDefault()
+    let s4 = () => {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+      }
+     ticket_id.innerHTML=s4()+s4();
+})
